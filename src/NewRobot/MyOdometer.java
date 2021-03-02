96f2899cadde1272aac9acb3e37070f92f2a3183
CarolinaRMarques <44268230+CarolinaRMarques@users.noreply.github.com>
2021-03-02 23:25:42 +0000

    Added Simple Robot and Odometer

---
+++ src/NewRobot/MyOdometer.java
@@ -0,0 +1,31 @@
+package NewRobot;
+import robocode.Robot;
+public class MyOdometer {
+	public Robot robot;
+	private double dist = 0.0;
+	private boolean is_racing;
+	private boolean finished;
+	
+	public MyOdometer (String name, Robot r) {
+		this.robot =r;
+		this.is_racing = false;
+		this.finished = false;
+	}
+	
+	public void race() {
+		this.is_racing = true;
+	}
+	
+	public void endRace() {
+		this.finished = true;
+		this.is_racing = false;
+	}
+	
+	public double getDistance(int x1, int x2, int y1, int y2) {
+        return  Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	}
+	
+	public String totalDist() {
+		return "Race Distance = X pixels";
+	}
+}
