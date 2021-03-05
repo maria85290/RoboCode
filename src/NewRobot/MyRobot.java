358cc65fb07a1f5e3bf7c8bcdce8a12beac31398
ConstancaElias <constanca.elias@gmail.com>
2021-03-05 10:39:15 +0000

    Add function goTo

--- src/NewRobot/MyRobot.java
+++ src/NewRobot/MyRobot.java
@@ -8,16 +8,16 @@ public class MyRobot extends AdvancedRobot{
 	private Odometer standardOdometer = new Odometer ("isRacing", this);
 	private MyOdometer odometer = new MyOdometer("isRacing",this);
 	private boolean start = false;
-	
+
 	double actualY;
 	double actualX;
 
-	public void run() {		
+	public void run() {
 		addCustomEvent(standardOdometer);
 		addCustomEvent(odometer);
-		
-		System.out.println(calcAngle(18,18));
-		setTurnGunLeft(calcAngle(18,18));
+
+		//setTurnGunRight(calcAngle(18,18));
+		turnRight(calcAngle(18,18));
 		ahead(100);
 	}
 
@@ -28,34 +28,56 @@ public class MyRobot extends AdvancedRobot{
 		if (cd.getName().equals("isRacing"))
 			this.standardOdometer.getRaceDistance();
 	}
-	
-	private void goTo() {
-		//ter atencao radar e arma ; aponta-los para (18,18) e ter angulos para essa posicao
-		this.actualX = this.getX();
-		this.actualY = this.getY();
+
+	private void goTo(double x, double y){
+		x = getX();
+		y = getY();
+
+		/* Calculate the angle to the target position */
+		double angleToTarget = Math.atan2(x, y);
+
+		/* Calculate the turn required get there */
+		double targetAngle = Utils.normalRelativeAngle(angleToTarget - getHeadingRadians());
+
+		/*
+		 * The Java Hypot method is a quick way of getting the length
+		 * of a vector. Which in this case is also the distance between
+		 * our robot and the target location.
+		 */
+		double distance = Math.hypot(x, y);
+
+		/* This is a simple method of performing set front as back */
+		double turnAngle = Math.atan(Math.tan(targetAngle));
+		setTurnRightRadians(turnAngle);
+		if(targetAngle == turnAngle) {
+			setAhead(distance);
+		} else {
+			setBack(distance);
+		}
 	}
-	
+
 	private void rotate() {
 		//Calcular angulos e apontar radar e arma para a posição
 
-		
+
 	}
-	
+
 	private double calcAngle(double destX, double destY) {
 		//Reta para obter o angulo
 		double distX = destX - actualX;
 		double distY = destY - actualY;
-		
+
 		//Angulo da reta 90º até à reta que vai da origem do robo até ao ponto(18,18)
 		double tempAngle = Math.atan2(distX, distY);
-		
-		System.out.println("Heading:" + this.getHeading());
-		
+
+		System.out.println("Atan angle: " + tempAngle);
+		System.out.println("Heading: " + this.getHeading());
+
 		//Subtração entre temp angle e o angulo da frente do robo
 		double rotationAngle = Utils.normalRelativeAngle(tempAngle- this.getHeading());
-		
-		
+
+		System.out.println("Rotation: " + rotationAngle);
 		return rotationAngle;
 	}
-		
+
 }
